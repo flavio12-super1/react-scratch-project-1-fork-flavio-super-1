7fa6629582ad303896664c89058a7f821bfd492a
hacker-flavio <theflavioherrera@gmail.com>
2023-12-07 21:20:28 -0800

    first commit

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+
+app.use(
+  express.static(path.join(__dirname, "../frontend/dist"), { index: false })
+);
+
+app.get("/", (req, res) => {
+  console.log("sending index.html");
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`server running on port: ${port}`);
+});
---
+++ frontend/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+};
