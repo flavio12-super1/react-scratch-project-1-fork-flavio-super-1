a6a7615cd66e4958f20406769ee2a6be1db1562d
hacker-flavio <theflavioherrera@gmail.com>
2023-12-18 16:27:53 -0800

    added animation

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import * as stylex from "@stylexjs/stylex";
+import tinycolor from "tinycolor2";
+
+const styles = stylex.create({
+  outerBar: (height) => ({
+    height: height,
+    transition: "height 0.3s ease, background-color 0.3s ease",
+  }),
+  bar: (settings) => ({
+    position: "relative",
+    overflow: "hidden",
+    height: settings.height,
+    backgroundColor: settings.color,
+    transition: "height 0.3s ease, background-color 0.3s ease",
+
+    ":hover": {
+      backgroundColor: tinycolor(settings.color).darken(20).toString(),
+      height: "100%",
+    },
+  }),
+});
 
 function Home() {
-  return <div>Home</div>;
+  const [settings, setSettings] = useState({
+    height: 50,
+    color: "rgb(50, 0, 200)",
+  });
+
+  const handleHeightChange = (event) => {
+    setSettings({ ...settings, height: parseInt(event.target.value, 10) });
+  };
+
+  const handleColorChange = (event) => {
+    setSettings({ ...settings, color: event.target.value });
+  };
+
+  return (
+    <div>
+      <div>
+        <label htmlFor="height">Select Height:</label>
+        <select
+          id="height"
+          onChange={handleHeightChange}
+          value={settings.height}
+        >
+          <option value={50}>50</option>
+          <option value={100}>100</option>
+          <option value={150}>150</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor="color">Select Color:</label>
+        <input
+          type="color"
+          id="color"
+          onChange={handleColorChange}
+          value={settings.color}
+        />
+      </div>
+      <div
+        {...stylex.props(styles.outerBar(settings.height))}
+        onClick={() =>
+          setSettings({
+            ...settings,
+            height:
+              settings.height >= 150
+                ? settings.height - 100
+                : settings.height + 100,
+          })
+        }
+      >
+        <div {...stylex.props(styles.bar(settings))} />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,7 +1,8 @@
 const path = require("path");
+const StylexPlugin = require("@stylexjs/webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
+const config = (env, argv) => ({
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -13,12 +14,13 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
+        // use: {
+        //   loader: "babel-loader",
+        //   options: {
+        //     presets: ["@babel/preset-env", "@babel/preset-react"],
+        //   },
+        // },
+        use: "babel-loader",
       },
     ],
   },
@@ -26,5 +28,27 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./index.html",
     }),
+    // Ensure that the stylex plugin is used before Babel
+    new StylexPlugin({
+      filename: "styles.[contenthash].css",
+      // get webpack mode and set value for dev
+      dev: argv.mode === "development",
+      // Use statically generated CSS files and not runtime injected CSS.
+      // Even in development.
+      runtimeInjection: false,
+      // optional. default: 'x'
+      classNamePrefix: "x",
+      // Required for CSS variable support
+      unstable_moduleResolution: {
+        // type: 'commonJS' | 'haste'
+        // default: 'commonJS'
+        type: "commonJS",
+        // The absolute path to the root directory of your project
+        rootDir: __dirname,
+      },
+    }),
   ],
-};
+  cache: true,
+});
+
+module.exports = config;
